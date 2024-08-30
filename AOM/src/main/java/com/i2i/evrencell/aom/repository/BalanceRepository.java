706c6752e4d1b472d2454384652939ba06baecb9
kaanyvz <kaan_yavuz12@hotmail.com>
2024-08-30 12:45:18 +0300

    refactoring repository codes

--- AOM/src/main/java/com/i2i/evrencell/aom/repository/BalanceRepository.java
+++ AOM/src/main/java/com/i2i/evrencell/aom/repository/BalanceRepository.java
@@ -13,129 +13,130 @@ import org.springframework.stereotype.Repository;
 import org.voltdb.client.ProcCallException;
 
 import java.io.IOException;
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.sql.Types;
+import java.sql.*;
 
 @Repository
 public class BalanceRepository {
     private final OracleConnection oracleConnection;
-    private final VoltdbOperator voltdbOperator = new VoltdbOperator();
+    private final VoltdbOperator voltdbOperator;
     private final Logger logger = LoggerFactory.getLogger(BalanceRepository.class);
 
-    public BalanceRepository(OracleConnection oracleConnection) {
+    public BalanceRepository(OracleConnection oracleConnection,
+                             VoltdbOperator voltdbOperator) {
         this.oracleConnection = oracleConnection;
+        this.voltdbOperator = voltdbOperator;
     }
 
-    /**
-     * Create balance for customer
-     * This method creates balance for customer in oracleDb
-     * It first gets the package details from oracleDb
-     * Then creates balance for customer with his package subscription
-     * @param createBalanceRequest
-     * @return
-     * @throws ClassNotFoundException
-     * @throws SQLException
-     */
+
     public ResponseEntity<String> createOracleBalance(CreateBalanceRequest createBalanceRequest) throws ClassNotFoundException, SQLException {
         logger.debug("Creating balance for customer with balanceId: {}", createBalanceRequest.balanceId());
         logger.debug("Creating balance for customer with customerId: {}",  createBalanceRequest.customerId());
         logger.debug("Creating balance for customer with packageId: {}",  createBalanceRequest.packageId());
 
         logger.debug("Connecting to OracleDb");
-        Connection connection = oracleConnection.getOracleConnection();
-        logger.debug("Connected to OracleDb");
+        try (Connection connection = oracleConnection.getOracleConnection();) {
 
-        logger.debug("Creating callable statement for SELECT_PACKAGE_DETAILS_ID");
-        CallableStatement packageCallableStatement = connection.prepareCall("{call SELECT_PACKAGE_DETAILS_ID(?, ?, ?, ?, ?)}");
-        packageCallableStatement.setInt(1, createBalanceRequest.packageId());
-        packageCallableStatement.registerOutParameter(2, Types.INTEGER);
-        packageCallableStatement.registerOutParameter(3, Types.INTEGER);
-        packageCallableStatement.registerOutParameter(4, Types.INTEGER);
-        packageCallableStatement.registerOutParameter(5, Types.INTEGER);
 
-        logger.debug("Executing SELECT_PACKAGE_DETAILS_ID");
-        packageCallableStatement.execute();
-        logger.debug("SELECT_PACKAGE_DETAILS_ID executed successfully");
+            logger.debug("Connected to OracleDb");
 
-        logger.debug("Getting package details from SELECT_PACKAGE_DETAILS_ID");
-        int amountMinutes = packageCallableStatement.getInt(2);
-        int amountSms = packageCallableStatement.getInt(3);
-        int amountData = packageCallableStatement.getInt(4);
-        int period = packageCallableStatement.getInt(5);
-        packageCallableStatement.close();
-        logger.debug("Package details retrieved successfully");
+            logger.debug("Creating callable statement for SELECT_PACKAGE_DETAILS_ID");
+            VoltPackageDetails voltPackageDetails = getOraclePackageDetails(connection, createBalanceRequest);
+            Timestamp[] balancePeriod = calculateBalancePeriod(voltPackageDetails.period());
 
-        if (amountMinutes == 0 && amountSms == 0 && amountData == 0 && period == 0) {
-            connection.close();
-            throw new NotFoundException("Package not found in oracleDb");
+            insertBalanceToOracle(createBalanceRequest, voltPackageDetails, balancePeriod, connection);
+            logger.debug("Balance created successfully.");
+
+            return new ResponseEntity<>("Balance created successfully", HttpStatus.CREATED);
         }
+    }
 
-        Timestamp sdate = new Timestamp(System.currentTimeMillis());
-        Timestamp edate = new Timestamp(sdate.getTime() + period * 24L * 60L * 60L * 1000L);
+    private VoltPackageDetails getOraclePackageDetails(Connection connection, CreateBalanceRequest request){
+        logger.debug("Retrieving package details for package id: {}", request.packageId());
+        try (CallableStatement balanceStmt = connection.prepareCall("{call SELECT_PACKAGE_DETAILS_ID(?, ?, ?, ?, ?)}")){
+            balanceStmt.setInt(1, request.packageId());
+            balanceStmt.registerOutParameter(2, Types.INTEGER);
+            balanceStmt.registerOutParameter(3, Types.INTEGER);
+            balanceStmt.registerOutParameter(4, Types.INTEGER);
+            balanceStmt.registerOutParameter(5, Types.INTEGER);
 
-        logger.debug("Creating callable statement for INSERT_BALANCE_TO_CUSTOMER");
-        CallableStatement balanceStmt = connection.prepareCall("{call INSERT_BALANCE_TO_CUSTOMER(?, ?, ?, ?, ?, ?, ?)}");
-        balanceStmt.setInt(1, createBalanceRequest.customerId());
-        balanceStmt.setInt(2, createBalanceRequest.packageId());
-        balanceStmt.setInt(3, amountMinutes);
-        balanceStmt.setInt(4, amountSms);
-        balanceStmt.setInt(5, amountData);
-        balanceStmt.setTimestamp(6, sdate);
-        balanceStmt.setTimestamp(7, edate);
-        logger.debug("Executing INSERT_BALANCE_TO_CUSTOMER");
-        balanceStmt.execute();
-        logger.debug("INSERT_BALANCE_TO_CUSTOMER executed successfully");
-        balanceStmt.close();
-        connection.close();
-        logger.debug("Balance created successfully in OracleDb");
+            balanceStmt.execute();
 
-        return new ResponseEntity<>("Balance created successfully", HttpStatus.CREATED);
+            int amountMinutes = balanceStmt.getInt(2);
+            int amountData = balanceStmt.getInt(3);
+            int amountSms = balanceStmt.getInt(4);
+            int period = balanceStmt.getInt(5);
+            if(amountMinutes == 0 && amountSms == 0 && amountData == 0 && period == 0){
+                throw new NotFoundException("Package not found in oracle.");
+            }
+            return new VoltPackageDetails(period, amountMinutes, amountSms, amountData);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void insertBalanceToOracle(CreateBalanceRequest request,
+                                       VoltPackageDetails voltPackageDetails,
+                                       Timestamp[] period,
+                                       Connection connection) throws SQLException {
+        logger.debug("Inserting balance to OracleDb for customerId: {}", request.customerId());
+        try (CallableStatement stmt = connection.prepareCall("{call INSERT_BALANCE_TO_CUSTOMER(?, ?, ?, ?, ?, ?, ?)}")) {
+            stmt.setInt(1, request.customerId());
+            stmt.setInt(2, request.packageId());
+            stmt.setInt(3, voltPackageDetails.amountMinutes());
+            stmt.setInt(4, voltPackageDetails.amountSms());
+            stmt.setInt(5, voltPackageDetails.amountData());
+            stmt.setTimestamp(6, period[0]);
+            stmt.setTimestamp(7, period[1]);
+            stmt.execute();
+        }
     }
 
 
+    private Timestamp[] calculateBalancePeriod(int period) {
+        Timestamp sdate = new Timestamp(System.currentTimeMillis());
+        Timestamp edate = new Timestamp(sdate.getTime() + period * 24L * 60L * 60L * 1000L);
+        return new Timestamp[]{sdate, edate};
+    }
+
     //==VOLTDB==
 
-    /**
-     * Create balance for customer
-     * This method creates balance for customer in voltDb
-     * It first gets the package details from voltDb with using packageId
-     * Then creates balance for customer with his package subscription
-     * @param createBalanceRequest
-     * @return
-     * @throws IOException
-     * @throws ProcCallException
-     */
+
     public ResponseEntity<String> createVoltBalance(CreateBalanceRequest createBalanceRequest) throws IOException, ProcCallException, InterruptedException {
 
         logger.debug("Creating balance for customer with balanceId: " + createBalanceRequest.balanceId());
         logger.debug("Creating balance for customer with customerId: " + createBalanceRequest.customerId());
         logger.debug("Creating balance for customer with packageId: " + createBalanceRequest.packageId());
         VoltPackageDetails voltPackageDetails = voltdbOperator.getPackageInfoByPackageId(createBalanceRequest.packageId());
+        Timestamp[] balancePeriod = calculateBalancePeriod(voltPackageDetails.period());
 
-        Timestamp sdate = new Timestamp(System.currentTimeMillis());
-        Timestamp edate = new Timestamp(sdate.getTime() + voltPackageDetails.period() * 24L * 60L * 60L * 1000L);
+        int balanceId = generateVoltBalanceId();
+        insertVoltBalance(createBalanceRequest, voltPackageDetails, balancePeriod, balanceId);
+        logger.debug("Balance created successfully in VOLTDB");
+        return new ResponseEntity<>("Balance created successfully in VOLTDB", HttpStatus.CREATED);
+    }
 
-        logger.debug("Getting max balance id from VOLTDB");
-        int maxBalanceId  = voltdbOperator.getMaxBalanceId();
-        int balanceId = maxBalanceId + 1;
-
-        logger.debug("Inserting balance to VOLTDB");
+    private void insertVoltBalance(CreateBalanceRequest request,
+                                   VoltPackageDetails packageDetails,
+                                   Timestamp[] period,
+                                   int balanceId){
+        logger.debug("Inserting balance to VOLTDB for customer id: {}", request.customerId());
         voltdbOperator.insertBalance(
                 balanceId,
-                createBalanceRequest.customerId(),
-                createBalanceRequest.packageId(),
-                voltPackageDetails.amountMinutes(),
-                voltPackageDetails.amountSms(),
-                voltPackageDetails.amountData(),
-                sdate,
-                edate
+                request.customerId(),
+                request.packageId(),
+                packageDetails.amountMinutes(),
+                packageDetails.amountSms(),
+                packageDetails.amountData(),
+                period[0],
+                period[1]
         );
-        logger.debug("Balance created successfully in VOLTDB");
+    }
 
-        return new ResponseEntity<>("Balance created successfully", HttpStatus.CREATED);
+    private int generateVoltBalanceId(){
+        logger.debug("Generating new balance id for VOLTDB");
+        int maxBalanceId = voltdbOperator.getMaxBalanceId();
+        return maxBalanceId + 1;
     }
 
 }
